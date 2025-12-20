e9f0c6e1a0d628d092aff965e9bd747fa008a8d8
bala-amypo <bala@amypo.in>
2025-12-20 17:22:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProgressRepository.java
+++ src/main/java/com/example/demo/repository/ProgressRepository.java
@@ -3,5 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.model.Progress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProgressRepository extends JpaRepository<Progress, Long> {
+
+    Optional<Progress> findByUserId(Long userId);
+
+    List<Progress> findByUserIdOrderByUpdatedAtDesc(Long userId);
 }

69882420b2c175eaa12633aab70296b665d61c4e
bala-amypo <bala@amypo.in>
2025-12-21 05:42:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Progress.java
+++ src/main/java/com/example/demo/model/Progress.java
@@ -1,28 +1,37 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.example.demo.model.User;
+import com.example.demo.model.MicroLesson;
+
+import java.io.Serializable;
+
 @Entity
 @Table(name = "progress")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Progress {
-
+public class Progress implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String status; // NOT_STARTED, IN_PROGRESS, COMPLETED
+    private String status;
     private Integer progressPercent;
     private LocalDateTime lastAccessedAt;
     private BigDecimal score;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonIgnoreProperties({"progresses","recommendations"})
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "micro_lesson_id", nullable = false)
-    @JsonIgnoreProperties({"progressList"})
     private MicroLesson microLesson;
 
     @PrePersist

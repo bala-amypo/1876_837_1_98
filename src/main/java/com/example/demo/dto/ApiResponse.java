306458d39733ad7e13d13e9addf557ef042e6c6f
bala-amypo <bala@amypo.in>
2025-12-21 09:30:14 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Generic API Response wrapper for consistent success/error responses.
+ */
 @Data
 @Builder
 @NoArgsConstructor

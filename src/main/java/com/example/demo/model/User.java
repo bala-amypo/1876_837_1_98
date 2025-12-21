c551f001c37be44c851a74cba9502eb7a1bc38ef
bala-amypo <bala@amypo.in>
2025-12-21 06:06:14 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,49 +1,19 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.*;
 
 @Entity
-@Table(name = "users")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fullName;
-
-    @Column(unique = true, nullable = false)
     private String email;
-
     private String password;
-
-    private String role; // LEARNER, INSTRUCTOR, ADMIN
-
-    private String preferredLearningStyle;
-
-    private LocalDateTime createdAt;
-
-    @OneToMany(mappedBy = "instructor")
-    private List<Course> courses;
-
-    @OneToMany(mappedBy = "user")
-    private List<Progress> progresses;
-
-    @OneToMany(mappedBy = "user")
-    private List<Recommendation> recommendations;
-
-    @PrePersist
-    public void prePersist() {
-        createdAt = LocalDateTime.now();
-        if (role == null) role = "LEARNER";
-    }
+    private String role;
 }

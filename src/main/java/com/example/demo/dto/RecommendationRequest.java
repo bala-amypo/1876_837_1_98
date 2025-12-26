e3d7ac8cbca963dd94e827c1e3899ae5df14a81d
bala-amypo <bala@amypo.in>
2025-12-26 07:59:44 +0000

    User commit

--- src/main/java/com/example/demo/dto/RecommendationRequest.java
+++ src/main/java/com/example/demo/dto/RecommendationRequest.java
@@ -1,9 +1,23 @@
+// package com.example.demo.dto;
+// import lombok.*;
+
+// @Data @Builder @AllArgsConstructor @NoArgsConstructor
+// public class RecommendationRequest {
+//     private String tags;
+//     private String difficulty;
+//     private String contentType;
+// }
 package com.example.demo.dto;
+
 import lombok.*;
 
-@Data @Builder @AllArgsConstructor @NoArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecommendationRequest {
     private String tags;
     private String difficulty;
     private String contentType;
+    private Integer limit;
 }

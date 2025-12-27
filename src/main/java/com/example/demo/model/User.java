cb209bebaa6a59014b4dec9636f08407e048db8a
bala-amypo <bala@amypo.in>
2025-12-27 03:50:06 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,30 +1,4 @@
-// package com.example.demo.model;
 
-// import jakarta.persistence.*;
-// import lombok.*;
-// import java.time.LocalDateTime;
-
-// @Entity
-// @Table(name = "users")
-// @Data
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class User {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private String email;
-//     private String password;
-//     private String role;
-//     private LocalDateTime createdAt;
-
-//     @PrePersist
-//     public void prePersist() {
-//         this.createdAt = LocalDateTime.now();
-//         if (this.role == null) this.role = "LEARNER";
-//     }
-// }
 package com.example.demo.model;
 
 import jakarta.persistence.*;
